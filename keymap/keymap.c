286d09187ff4501c61530d8835855a5086d88e1b
joelklevje <klevjej@gmail.com>
2023-11-06 18:16:04 +0100

    swap ctrl and gui

--- keymap/keymap.c
+++ keymap/keymap.c
@@ -47,7 +47,7 @@ enum custom_keycodes {
 };
 
 // Home row mods
-#define HRM_A LCTL_T(KC_A)
+#define HRM_A LGUI_T(KC_A)
 #define HRM_R LALT_T(KC_R)
 #define HRM_S LT(NUM, KC_S)
 #define HRM_T LSFT_T(KC_T)
@@ -55,13 +55,13 @@ enum custom_keycodes {
 #define HRM_N RSFT_T(KC_N)
 #define HRM_E LT(OPS, KC_E)
 #define HRM_I RALT_T(KC_I)
-#define HRM_O RCTL_T(KC_O)
+#define HRM_O RGUI_T(KC_O)
 
 #define MY_D LT(MED, KC_D)
 
 // Thumb cluster
-#define TMB_TAB LGUI_T(KC_TAB)
-#define TMB_ENT RGUI_T(KC_ENT)
+#define TMB_TAB LCTL_T(KC_TAB)
+#define TMB_ENT RCTL_T(KC_ENT)
 #define TMB_SPC LT(SYM, KC_SPC)
 #define NAV_REP LT(NAV, KC_BSPC)
 
